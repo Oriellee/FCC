0e3baab8168b49b191faece727ae7f1478393bd6
Oriel <1102386151@qq.com>
2017-09-25 19:54:48 +0800

    1.

--- 观看直播平台/main.js
+++ 观看直播平台/main.js
@@ -18,21 +18,29 @@ $(function(){
 			},400);
 		
 	}
-	$(".all,.online,.offline").hover(function(event) {
+	$(".noActive").hover(function(event) {
 		move($(this));
 	});
-	$(".all").click(function(event) {
-		$(".on,.off").css('display', 'inline-block');
-		move($(this));
+	$(".all,.online,.offline").click(function(event) {
+		$(this)
+		.addClass('active')
+		.removeClass('noActive')
+		.siblings()
+		.removeClass('active')
+		.addClass('noActive');
 	});
-	$(".online").click(function(event) {
-		$(".on").css('display', 'inline-block');
-		$(".off").css('display', 'none');
-		move($(this));
-	});
-	$(".offline").click(function(event) {
-		$(".on").css('display', 'none');
-		$(".off").css('display', 'inline-block');
-		move($(this));
-	});
+	// $(".all").click(function(event) {
+	// 	$(".on,.off").css('display', 'inline-block');
+	// 	move($(this));
+	// });
+	// $(".online").click(function(event) {
+	// 	$(".on").css('display', 'inline-block');
+	// 	$(".off").css('display', 'none');
+	// 	move($(this));
+	// });
+	// $(".offline").click(function(event) {
+	// 	$(".on").css('display', 'none');
+	// 	$(".off").css('display', 'inline-block');
+	// 	move($(this));
+	// });
 })
