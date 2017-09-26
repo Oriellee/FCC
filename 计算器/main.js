219222bc487a73301c51e1d6914a974191bedc52
Oriel <1102386151@qq.com>
2017-09-26 19:37:19 +0800

    完成。

--- 计算器/main.js
+++ 计算器/main.js
@@ -1,9 +1,36 @@
-$(function(){
-	$("#box table tr td").click(function(event) {
-		var num = $(this).text();
-		if($.isNumeric(num)){
-			$("#show").text(num);
-		}else{}
-		
-	});
+var ans = "",
+    calc = "",
+    clear = false;
+
+$(function() {
+    $("#box table tr td").click(function(event) {
+        var text = $(this).text();
+        var show = $("#show");
+        if (parseInt(text, 10) == text ||
+            text === "%" ||
+            text === "/" ||
+            text === "*" ||
+            text === "-" ||
+            text === "+" ||
+            text === ".") {
+        	if(clear === false){
+        		calc+=text;
+        		show.text(calc);
+        	}else{
+        		calc = text;
+        		show.text(calc);
+        		clear = false;
+        	}
+        }else if(text === "AC"){
+        	calc = "";
+        	show.text("");
+        }else if(text === "CE"){
+        	calc = calc.slice(0, -1);
+        	show.text(calc);
+        }else if(text === "="){
+        	ans = eval(calc);
+        	show.text(ans);
+        	clear = true;
+        }
+    });
 })
